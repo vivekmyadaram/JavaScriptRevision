294c55db99036f038a7885094488003e19763d89
vivek <vivekvishwakarma0719@gmail.com>
2024-01-07 01:12:17 +0530

    checking git push or not

---
+++ 07Jan/data-types.js
@@ -0,0 +1,7 @@
+/*data types in javascript let, const, var are used to declare variables, but they have some differences in terms of scope, hoisting and re - assignment
+
+Var
+----
+Scope: var is function-scoped. it's hoisted to the top of function scope
+
+*/
